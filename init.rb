0cc0e6d36c350aaf5ac9ea5625ff35a556ccf2e7
Kota Shiratsuka <kota@insaneworks.co.jp>
2016-04-01 14:08:35 +0900

    delete recive_patch

--- init.rb
+++ init.rb
@@ -1,17 +1,14 @@
 require 'htmlentities'
 coder = HTMLEntities.new
-INVISIBLE_EMAIL_HEADER = "&#8203;" * 20
-INVISIBLE_EMAIL_HEADER_DECODED = coder.decode(INVISIBLE_EMAIL_HEADER)
 FIND_IMG_SRC_PATTERN = /(<img[^>]+src=")((?:#{Setting.protocol + "://" + Setting.host_name + Redmine::Utils.relative_url_root})[^"]+)("[^>]*>)/
 
 require 'email_send_patch'
-require 'email_receive_inline_patch'
 
 Redmine::Plugin.register :redmine_email_images do
   name 'Redmine Email Images plugin'
   author 'Dmitriy Kalachev'
   description 'Send images as attachments instead of using URL and getting a 403 error in email clients.'
-  version '0.1.2'
+  version '0.2.0'
   url 'http://github.com/dkalachov/redmine_email_images'
   author_url 'http://dkalachov.com'
 end
